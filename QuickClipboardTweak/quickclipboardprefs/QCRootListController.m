09d491478702b6a1c1691cf6b5a8fe236ae7a31f
lvxl524 <106232392+lvxl524@users.noreply.github.com>
2026-08-11 22:31:04 +0800

    v1.2.1: Version bump + settings version display
    - Version: 1.2.0 -> 1.2.1 (control + QC_VERSION)
    - Settings page now shows version label at bottom
    - Deleted old v1.2.0 release (had stale deb)

--- QuickClipboardTweak/quickclipboardprefs/QCRootListController.m
+++ QuickClipboardTweak/quickclipboardprefs/QCRootListController.m
@@ -1,16 +1,17 @@
 #import "QCRootListController.h"
 #import "QCWebDAVController.h"
 #import "QCLANController.h"
+#import "../QuickClipboard.h"
 
 @interface QCRootListController ()
 @property (nonatomic, strong) UISegmentedControl *segmentedControl;
 @property (nonatomic, strong) QCWebDAVController *webDAVController;
 @property (nonatomic, strong) QCLANController *lanController;
+@property (nonatomic, strong) UILabel *versionLabel;
 @end
 
 @implementation QCRootListController
 
-// Return empty specifiers to satisfy PSListController contract
 - (id)specifiers {
     return @[];
 }
@@ -18,6 +19,7 @@
 - (void)viewDidLoad {
     self.title = @"QuickClipboard";
 
+    // Segmented control in nav bar
     self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"WebDAV", @"局域网"]];
     [self.segmentedControl addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
     self.segmentedControl.selectedSegmentIndex = 0;
@@ -32,6 +34,27 @@
     [self.view addSubview:self.webDAVController.view];
     self.webDAVController.view.frame = self.view.bounds;
     self.webDAVController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+    // Version label at the bottom
+    [self setupVersionLabel];
+}
+
+- (void)setupVersionLabel {
+    self.versionLabel = [[UILabel alloc] init];
+    self.versionLabel.text = [NSString stringWithFormat:@"QuickClipboard v%@", QC_VERSION];
+    self.versionLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightMedium];
+    self.versionLabel.textColor = [UIColor grayColor];
+    self.versionLabel.textAlignment = NSTextAlignmentCenter;
+    self.versionLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.versionLabel];
+
+    // Pin to bottom with a safe area inset
+    [NSLayoutConstraint activateConstraints:@[
+        [self.versionLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.versionLabel.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-12],
+        [self.versionLabel.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.view.leadingAnchor constant:16],
+        [self.versionLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.view.trailingAnchor constant:-16]
+    ]];
 }
 
 - (void)segmentChanged:(UISegmentedControl *)sender {
@@ -46,6 +69,8 @@
         self.lanController.view.frame = self.view.bounds;
         self.lanController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     }
+    // Bring version label to front
+    [self.view bringSubviewToFront:self.versionLabel];
 }
 
 @end
